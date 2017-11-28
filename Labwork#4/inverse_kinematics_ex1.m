c363f9903e8722bdff194565aa596c3fb2384ac9
Paulo Almeida <paulo.cruz1920@gmail.com>
2017-11-28 15:34:45 +0000

    cinemática inversa ex1

---
+++ Labwork#4/inverse_kinematics_ex1.m
@@ -0,0 +1,27 @@
+%% Função da Cinemática Inversa do Robot
+%  Consultar inverse_kinematics_ex1.pdf, para melhor compreensão da implementação
+
+function [ q ] = inverse_kinematics_ex1(oTg)
+
+% Vector t
+tx = oTg(1,4);
+ty = oTg(2,4);
+
+% vector a
+ax = oTg(1,3);
+ay = oTg(2,3);
+
+% expressões para a cinemática inversa:
+
+theta1 = atan2( ty - 10 * ay, tx - 10 * ax);
+
+d2 = cos(theta1) * (tx - 10 * ax) + sin(theta1) * (ty - 10 * ay); 
+
+theta3 = atan2(ay,ax) - theta1;
+
+% vector com parâmetros das juntas:
+
+q = [ theta1 d2 theta3 ];
+
+
+end
