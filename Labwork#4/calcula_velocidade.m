fcc37a30de1672c318b7003c2505ac27ad3b9dd6
Paulo Almeida <paulo.cruz1920@gmail.com>
2017-12-14 02:41:49 +0000

    acabei calcula_velocidade.m (falta testar)

--- Labwork#4/calcula_velocidade.m
+++ Labwork#4/calcula_velocidade.m
@@ -1,11 +1,15 @@
 function [velocidade_juntas] = calcula_velocidade(theta, T)
 
-
+% casos particulares:
+    % velocidade das juntas no instante inicial e final:
+    
+velocidade_juntas(1,:) = 0;
+velocidade_juntas(size(T),:) = 0;
 
 for i = 2 : 1 : (size(T)-1) % percorre os troços da trajectória
     for k = 1 : 1 : size(theta) % percorre as juntas do manipulador
         
-        if sign(q_juntas(i,k) - q_juntas(i,k-1)) ~= sign(q_juntas(i,k+1) - q_juntas(i,k))
+        if sign(q_juntas(i,k) - q_juntas(i,k-1)) ~= sign(q_juntas(i,k+1) - q_juntas(i,k)) % se houver inversão do sinal
             
             velocidade_juntas = 0;
         
